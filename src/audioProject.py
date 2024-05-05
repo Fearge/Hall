f9938e5f038343e411e86790c614770420ca7976
Fearge <mario1.seibert@web.de>
2024-05-05 14:35:12 +0200

    ausprobiert, funktioniert

--- src/audioProject.py
+++ src/audioProject.py
@@ -1,11 +1,21 @@
 import scipy.io.wavfile as wavfile
 from scipy import signal
-rate1, x = wavfile.read('spoken.wav')
+
+#TODO:Abfragen, ob eigene Datei oder preset
+
+    #TODO:Datei einlesen
+
+    #TODO:Preset Dateien auswählen
+
+#Wavfile und IR einlesen
+rate1, x = wavfile.read('C:/Users/Fearge/Downloads/Audioprojekt/spoken.wav')
 rate2, h = wavfile.read('big_hall.wav')
+
 assert(rate1 == rate2)
 x = x.mean(axis=1) if len(x.shape) > 1 else x
 h = h.mean(axis=1) if len(h.shape) > 1 else x
-#Falsung
+
+#Faltung
 y = signal.fftconvolve(x, h)
 #Skalierung auf +-32765 und umwandeln in Integer
 y /= max(abs(y))
