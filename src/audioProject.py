b2d071fb745a5c1dc5ea046ae059d6b56ef658a8
wasgehtey <linus.ollmann@haw-hamburg.de>
2024-05-09 17:24:52 +0200

    Hallo

--- src/audioProject.py
+++ src/audioProject.py
@@ -25,10 +25,10 @@ if __name__ == '__main__':
         elif (eingabe.upper() == "P"):
             eingabe = input("Wähle ein Preset. Preset 1 (classroom.wav) = 1, Preset 2 (big_hall.wav) = 2 \nBitte Wahl eintippen: ")
 
-            if(eingabe == 1):
+            if(eingabe == "1"):
                 impulsAntwort = 'classroom.wav'
 
-            elif(eingabe == 2):
+            elif(eingabe == "2"):
                 impulsAntwort = 'big_hall.wav'
 
     elif (eingabe.upper() == "P"):
@@ -48,6 +48,7 @@ if __name__ == '__main__':
 
     elif (eingabe.upper() == "A"):
         functions.ausgabe(gefalteteDatei, sampleRate, 0)
+
 
 
 
