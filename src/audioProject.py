8fc77e918011ec9329e529463f42c46822b5907c
Rainjam47 <map_p@outlook.de>
2024-05-09 15:11:20 +0200

    BenutzerEingabeFuerSpeichern

--- src/audioProject.py
+++ src/audioProject.py
@@ -32,33 +32,6 @@ def convolve(file1, file2):
     normalizeGefaltet = functions.normalize(gefaltet)
     return normalizeGefaltet, rate1
 
-# Metadaten der Datei ausgeben
-def showStats(file):
-
-    roomfn = file
-    rate, data = wavfile.read(roomfn)
-    N = data.shape[0]
-    CHN = data.shape[1] if len(data.shape) == 2 else 1
-
-    print(f"Audiodatei {roomfn} mit Abtastrate {rate}Hz")
-    print(f"Anzahl der Abtastwerte in der Datei: {N}")
-    print(f"Anzahl der Audio-Kanäle (1=Mono, 2=Stereo): {CHN}")
-    print(f"Dauer: {N / rate:.3f}s")
-    print("Die ersten Vier Abtastwerte:")
-    print(data[:4])
-
-# Ergebnis ausgeben, entweder auf Soundkarte oder als .wav
-def ausgabe(fileToPrint,rateToPrint,isSpeichern):
-    # als .wav
-    if(isSpeichern):
-        wavfile.write(input("Bitte geben Sie einen Namen für die zu speichernde Datei an: ") + ".wav", rateToPrint, fileToPrint)
-    else:
-        sd.play(fileToPrint,rateToPrint)
-        input("Drücke die Eingabetaste, um den Sound anzuhalten")
-        sd.stop()
-
-
-
 if __name__ == '__main__':
 
     eingabe = input("Hallo :) Möchtest du eigene Dateien mit Hall belegen oder welche von unseren Presets nutzen? (P = Presets / E = Eigene) \nBitte Wahl eintippen: ")
--- src/functions.py
+++ src/functions.py
@@ -1,38 +1,34 @@
 import scipy.io.wavfile as wavfile
 from scipy import signal
 
-
-def read_file(filename):
-    rate, amp_list = wavfile.read(filename)
-    return rate, amp_list
-
-
-def get_mono(amp_list):
-    amp_list = amp_list.mean(axis=1) if len(amp_list.shape) > 1 else amp_list
-    return amp_list
-
-
-def convolve(amp_signal, amp_response):
-    wet_signal = signal.fftconvolve(amp_signal, amp_response)
-    return wet_signal
-
-
 def normalize(amp_list):
     amp_list /= max(abs(amp_list))
     amp_list *= (2 ** 15 - 1)
     amp_list = amp_list.astype('int16')
     return amp_list
 
+#  Metadaten der Datei ausgeben
+def showStats(file):
 
-def write_file(amp_list, filename, rate):
-    wavfile.write(filename, rate, amp_list)
-
-def show_stats(rate,data,filename):
-
+    roomfn = file
+    rate, data = wavfile.read(roomfn)
     N = data.shape[0]
     CHN = data.shape[1] if len(data.shape) == 2 else 1
-    print(f"Audiodatei {filename} mit Abtastrate {rate} Hz")
+
+    print(f"Audiodatei {roomfn} mit Abtastrate {rate}Hz")
     print(f"Anzahl der Abtastwerte in der Datei: {N}")
-    print(f"Anzahl der Audio-Kanäle(1=Mono, 2=Stereo): {CHN}")
-    print("Die ersten vier Abtastwerte:")
+    print(f"Anzahl der Audio-Kanäle (1=Mono, 2=Stereo): {CHN}")
+    print(f"Dauer: {N / rate:.3f}s")
+    print("Die ersten Vier Abtastwerte:")
     print(data[:4])
+
+
+# Ergebnis ausgeben, entweder auf Soundkarte oder als .wav
+def ausgabe(fileToPrint,rateToPrint,isSpeichern):
+    # als .wav
+    if(isSpeichern):
+        wavfile.write(input("Bitte geben Sie einen Namen für die zu speichernde Datei an: ") + ".wav", rateToPrint, fileToPrint)
+    else:
+        sd.play(fileToPrint,rateToPrint)
+        input("Drücke die Eingabetaste, um den Sound anzuhalten")
+        sd.stop()
